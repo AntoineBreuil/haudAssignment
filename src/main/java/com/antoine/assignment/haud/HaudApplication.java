7a50d89f085aa9d44a2128c21dec28e27c01e954
AntoineBreuil <42042255+AntoineBreuil@users.noreply.github.com>
2020-12-05 07:13:34 +0100

    Initial commit

---
+++ src/main/java/com/antoine/assignment/haud/HaudApplication.java
@@ -0,0 +1,13 @@
+package com.antoine.assignment.haud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaudApplication.class, args);
+    }
+
+}
